30c86ae3bb04a325b83b07b07efec056f9de7a0a
Joseph <10326622@upsamail.edu.gh>
2025-11-28 19:20:03 +0100

    Initial commit

---
+++ alx-project-0x04/components/layouts/Layout.tsx
@@ -0,0 +1,17 @@
+import { type LayoutProps } from "@/interfaces"
+import Footer from "./Footer"
+import Header from "./Header"
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="pt-24">{children}</main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
+// started coding
+// start
